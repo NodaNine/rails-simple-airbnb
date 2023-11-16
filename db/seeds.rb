7e5964189ddf5ad60ffbcfbd7e13f0a8ee142319
Noemie Dalex <noemie.dalex@gmail.com>
2023-11-16 10:44:37 +0100

    style unfinished, main pages up and working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+maison = {name: "Maison du bonheur", address: "7 Boundary St, London E2 7JE", description: "ideal for 3", price_per_night: 75, number_of_guests: 3}
+tarata = {name: "Tarata's", address: "120 rue de Vaugirard", description: "ideal for a family, quiet", price_per_night: 150, number_of_guests: 5}
+
+[maison, tarata].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
